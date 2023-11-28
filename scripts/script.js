3a23ae33ee0e9b903b83d3af2bd8be7abfb9efec
Purpup <samil.ahsan@icloud.com>
2023-11-27 23:54:09 -0800

    fixed masking, added dynamic character limiter

--- scripts/script.js
+++ scripts/script.js
@@ -8,75 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let errorOutput = document.getElementById('errorOutput');
     let infoOutput = document.getElementById('infoOutput');
-    
 
-    nameInput.addEventListener('input', function() {
-        let nameValue = nameInput.value.trim();
-        if(nameValue.length > 0) {
-            nameInput.setCustomValidity('');
-            nameError.textContent = '';
-            errorOutput.textContent = '';
-        }
-        else {
-            nameInput.setCustomValidity('Please enter your name.');
-            showError(nameError, nameInput.validationMessage);
-            // errorOutput.textContent = 'Please fix errors in the form.';
-        }
-        // validate();
-    });
-
-    emailInput.addEventListener('input', function () {
-        let emailValue = emailInput.value.trim();
-        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (emailPattern.test(emailValue)) {
-            // The email format is valid, clear any previous error message
-            emailInput.setCustomValidity('');
-            emailError.textContent = '';
-            errorOutput.textContent = '';
-        } 
-        else {
-            // The email format is invalid, set a custom error message
-            emailInput.setCustomValidity('Please enter a valid email address.');
-            showError(emailError, emailInput.validationMessage);
-            // errorOutput.textContent = 'Please fix errors in the form.';
-        }
-        // validate();
-    });
-
-    commentsInput.addEventListener('input', function() {
-        let commentsValue = commentsInput.value.trim();
-        if(commentsValue.length > 0) {
-            commentsInput.setCustomValidity('');
-            commentsError.textContent = '';
-            errorOutput.textContent = '';
-        }
-        else {
-            commentsInput.setCustomValidity('Please enter some comments.');
-            showError(commentsError, commentsInput.validationMessage);
-            // errorOutput.textContent = 'Please fix errors in the form.';
-        }
-        // validate();
-    });
-
-    function validate() {
-        let nameValue = nameInput.value.trim();
-        let emailValue = emailInput.value.trim();
-        let commentsValue = commentsInput.value.trim();
-        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if(nameValue.length > 0 && emailPattern.test(emailValue) && commentsValue.length > 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    function showError(target, message) {
-        // Display the error message
-        target.textContent = message;
-    }
+    let formErrors = [];
 
     function mask(element, regex, error) {
         element.addEventListener('input', function() {
@@ -96,19 +29,104 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
-    //mask(nameInput, /[^a-zA-Z\s]/, nameError);
+    mask(nameInput, /[^a-zA-Z\s]/, errorOutput);
+    mask(emailInput, /[^\w@.]/, errorOutput);
+    mask(commentsInput, /[^a-zA-Z\s\d.,?!]/, errorOutput);
+    
 
-    let contactForm = document.getElementById('contactForm');
-    contactForm.addEventListener('submit', function(event) {
-        let valid = validate();
-        if(!valid) {
-            event.preventDefault();
-            errorOutput.textContent = 'Please fix the errors in the form.';
+    nameInput.addEventListener('input', function() {
+        let nameValue = nameInput.value.trim();
+        if(nameValue.length > 0) {
+            nameInput.setCustomValidity('');
+            nameError.textContent = '';
+            errorOutput.textContent = '';
         }
         else {
-            errorOutput.textContent = '';
+            nameInput.setCustomValidity('Please enter your name.');
+            showError(nameError, nameInput.validationMessage);
         }
     });
 
+    emailInput.addEventListener('input', function () {
+        let emailValue = emailInput.value.trim();
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (emailPattern.test(emailValue)) {
+            emailInput.setCustomValidity('');
+            emailError.textContent = '';
+            errorOutput.textContent = '';
+        } 
+        else {
+            emailInput.setCustomValidity('Please enter a valid email address.');
+            showError(emailError, emailInput.validationMessage);
+        }
+    });
+
+    commentsInput.addEventListener('input', function() {
+        let commentsValue = commentsInput.value.trim();
+        let remaining = 500 - commentsValue.length;
+        if(commentsValue.length > 0) {
+            commentsInput.setCustomValidity('');
+            commentsError.textContent = '';
+            errorOutput.textContent = '';
+        }
+        else {
+            commentsInput.setCustomValidity('Please enter some comments.');
+            showError(commentsError, commentsInput.validationMessage);
+        }
+        if(remaining >= 0) {
+            infoOutput.textContent = 'Characters remaining: ' + remaining;
+            if(remaining > 25) {
+                infoOutput.style.color = getComputedStyle(document.documentElement)
+                .getPropertyValue('--primary-color');
+            }
+            else {
+                infoOutput.style.color = 'red';
+            }
+        }
+        else {
+            commentsInput.setCustomValidity('Character limit exceeded.');
+            showError(commentsError, commentsInput.validationMessage);
+            commentsInput.readOnly = true;
+        }
+    });
+
+    function validate() {
+        let nameValue = nameInput.value.trim();
+        let emailValue = emailInput.value.trim();
+        let commentsValue = commentsInput.value.trim();
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(nameValue.length > 0 && emailPattern.test(emailValue) && commentsValue.length > 0) {
+            formErrors = [];
+            submitErrors();
+            return true;
+        }
+        else {
+            submitErrors();
+            return false;
+        }
+    }
+
+    function submitErrors() {
+        if(formErrors.length > 0) {
+            let data = JSON.stringify(formErrors);
+            let hidden = document.createElement('input');
+
+            hidden.type = 'hidden';
+            hidden.name = 'form-errors';
+            hidden.value = data;
+
+            document.getElementsById('contactForm').appendChild(hidden);
+        }
+    }
+
+    function addError(target, message) {
+        formErrors.push({ field: target.previousElementSibling.getAttribute('for'), message: message });
+    }    
+
+    function addError(target, message) {
+        formErrors.push({ field: target.previousElementSibling.getAttribute('for'), message: message });
+    }
 
 });
